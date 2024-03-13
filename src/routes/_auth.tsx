90e34e1955421a3890c32cae392f3d73dd721052
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-03-13 19:20:28 -0300

    feat: implement initial sign in structure

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -4,8 +4,14 @@ export const Route = createFileRoute('/_auth')({ component: AuthLayout })
 
 function AuthLayout() {
 	return (
-		<div>
-			<p>Auth Layout</p>
+		<div className="min-h-screen md:grid grid-cols-2">
+			<section className="hidden h-full bg-muted p-10 text-muted-foreground md:flex flex-col justify-between">
+				<header>we coffee</header>
+
+				<footer>
+					Painel do parceiro &copy; we coffee - {new Date().getFullYear()}
+				</footer>
+			</section>
 
 			<Outlet />
 		</div>
--- src/routes/_auth/sign-in.tsx
+++ src/routes/_auth/sign-in.tsx
@@ -1,10 +1,34 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { createFileRoute } from '@tanstack/react-router'
+import { Helmet } from 'react-helmet-async'
 
 function SignIn() {
 	return (
-		<div>
-			<h1>Sign In</h1>
-		</div>
+		<>
+			<Helmet title="Sign in" />
+
+			<main className="grid place-content-center gap-6 p-8">
+				<section>
+					<h1 className="text-lg md:text-2xl font-semibold tracking-tight">
+						Acessar o painel
+					</h1>
+					<p className="text-sm text-muted-foreground">
+						Acompanhe suas vendas pelo painel do parceiro!
+					</p>
+				</section>
+
+				<form className="space-y-4">
+					<div className="space-y-2">
+						<Label htmlFor="email">Seu e-mail</Label>
+						<Input id="email" type="email" />
+					</div>
+
+					<Button className="w-full">Acessar</Button>
+				</form>
+			</main>
+		</>
 	)
 }
 
