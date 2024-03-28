42a2c67860c1ed3be1efde14954bde0927c8c9bc
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-03-28 06:25:36 -0300

    feat: initial form management
    Implement initial structure for handling forms.
    With addition of Sooner component for better feedback.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { Toaster } from 'sonner'
 
 export const Route = createRootRoute({
 	component: () => (
 		<HelmetProvider>
 			<Helmet titleTemplate="%s | We Coffee" />
+      <Toaster richColors />
 			<Outlet />
 		</HelmetProvider>
 	),
--- src/routes/_auth/sign-in.tsx
+++ src/routes/_auth/sign-in.tsx
@@ -1,31 +1,58 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { createFileRoute } from '@tanstack/react-router'
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { toast } from 'sonner'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 function SignIn() {
-	return (
+	const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+
+  const handleSignIn = async (data: SignInForm) => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      toast.success('Foi encaminhado um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn
+        }
+      })
+    } catch {
+      toast.error('E-mail não encontrado.')
+    }
+  }
+
+  return (
 		<>
 			<Helmet title="Sign in" />
 
 			<main className="grid place-content-center gap-6 p-8">
 				<section>
-					<h1 className="text-lg md:text-2xl font-semibold tracking-tight">
+					<h1 className='font-semibold text-lg tracking-tight md:text-2xl'>
 						Acessar o painel
 					</h1>
-					<p className="text-sm text-muted-foreground">
+					<p className='text-muted-foreground text-sm'>
 						Acompanhe suas vendas pelo painel do parceiro!
 					</p>
 				</section>
 
-				<form className="space-y-4">
+				<form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
 					<div className="space-y-2">
 						<Label htmlFor="email">Seu e-mail</Label>
-						<Input id="email" type="email" />
+						<Input id="email" type="email" {...register('email')} />
 					</div>
 
-					<Button className="w-full">Acessar</Button>
+					<Button disabled={isSubmitting} className="w-full">Acessar</Button>
 				</form>
 			</main>
 		</>
