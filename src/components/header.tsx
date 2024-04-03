e97c60bd2d8fe88019071624ff164ed2b6decd51
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-04-03 20:38:51 -0300

    feat: implement theme switcher
    User can alternate between following themes: light, dark and system.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Coffee, Home } from 'lucide-react'
 import { Separator } from './ui/separator'
 import { Link } from '@tanstack/react-router'
+import { ThemeToggle } from './theme/theme-toggle'
 
 export function Header() {
 	return (
@@ -13,19 +14,23 @@ export function Header() {
 				<nav className="flex items-center gap-4 lg:gap-6">
 					<Link
 						to="/"
-						className='flex items-center gap-2 font-medium text-muted-foreground text-sm data-[status=active]:text-foreground hover:text-foreground'
+						className="flex items-center gap-2 font-medium text-muted-foreground text-sm data-[status=active]:text-foreground hover:text-foreground"
 					>
 						<Home className="size-5" />
 						Home
 					</Link>
 					<Link
 						to="/orders"
-						className='flex items-center gap-2 font-medium text-muted-foreground text-sm data-[status=active]:text-foreground hover:text-foreground'
+						className="flex items-center gap-2 font-medium text-muted-foreground text-sm data-[status=active]:text-foreground hover:text-foreground"
 					>
 						<Coffee className="size-5" />
 						Pedidos
 					</Link>
 				</nav>
+
+				<div className="ml-auto flex items-center gap-2">
+					<ThemeToggle />
+				</div>
 			</div>
 		</header>
 	)
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,12 +2,16 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from '@/components/theme/theme-provider'
+
 export const Route = createRootRoute({
 	component: () => (
 		<HelmetProvider>
-			<Helmet titleTemplate="%s | We Coffee" />
-      <Toaster richColors />
-			<Outlet />
+			<ThemeProvider storageKey="wecoffee-theme">
+				<Helmet titleTemplate="%s | We Coffee" />
+				<Toaster richColors />
+				<Outlet />
+			</ThemeProvider>
 		</HelmetProvider>
 	),
 })
