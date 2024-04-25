4117492147409c942cdd2437b436b6a4cd9d3975
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-04-25 09:39:30 -0300

    feat: connect to popular products endpoint

--- src/components/dashboard/popular-products-chart.tsx
+++ src/components/dashboard/popular-products-chart.tsx
@@ -6,14 +6,8 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
-
-const DATA = [
-	{ product: 'Oreo Shake', amount: 73 },
-	{ product: 'Croissant Bacon', amount: 61 },
-	{ product: 'Flat Croissant Chocolate', amount: 47 },
-	{ product: 'Caramelo Macchiato', amount: 39 },
-	{ product: 'Framboesa Duo Salty Cream', amount: 23 },
-]
+import { useQuery } from '@tanstack/react-query'
+import { getPopularProducts } from '@/api/get-popoular-products'
 
 const COLORS = [
 	{
@@ -39,6 +33,11 @@ const COLORS = [
 ]
 
 export function PopularProductsChart() {
+	const { data: popularProducts } = useQuery({
+		queryKey: ['metrics', 'popular-products'],
+		queryFn: getPopularProducts,
+	})
+
 	const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
 		? 'dark'
 		: 'light'
@@ -53,58 +52,62 @@ export function PopularProductsChart() {
 			</CardHeader>
 
 			<CardContent>
-				<ResponsiveContainer width="100%" height={240}>
-					<PieChart>
-						<Pie
-							data={DATA}
-							nameKey="product"
-							dataKey="amount"
-							cx="50%"
-							cy="50%"
-							innerRadius={64}
-							outerRadius={86}
-							strokeWidth={8}
-							labelLine={false}
-							label={({
-								cx,
-								cy,
-								midAngle,
-								innerRadius,
-								outerRadius,
-								value,
-								index,
-							}) => {
-								const RADIAN = Math.PI / 180
-								const radius = 12 + innerRadius + (outerRadius - innerRadius)
-								const x = cx + radius * Math.cos(-midAngle * RADIAN)
-								const y = cy + radius * Math.sin(-midAngle * RADIAN)
+				{popularProducts && (
+					<ResponsiveContainer width="100%" height={240}>
+						<PieChart>
+							<Pie
+								data={popularProducts}
+								nameKey="product"
+								dataKey="amount"
+								cx="50%"
+								cy="50%"
+								innerRadius={64}
+								outerRadius={86}
+								strokeWidth={8}
+								labelLine={false}
+								label={({
+									cx,
+									cy,
+									midAngle,
+									innerRadius,
+									outerRadius,
+									value,
+									index,
+								}) => {
+									const RADIAN = Math.PI / 180
+									const radius = 12 + innerRadius + (outerRadius - innerRadius)
+									const x = cx + radius * Math.cos(-midAngle * RADIAN)
+									const y = cy + radius * Math.sin(-midAngle * RADIAN)
 
-								return (
-									<text
-										x={x}
-										y={y}
-										className="fill-muted-foreground text-xs"
-										textAnchor={x > cx ? 'start' : 'end'}
-										dominantBaseline="central"
-									>
-										{DATA[index].product.length > 12
-											? DATA[index].product.substring(0, 12).concat('...')
-											: DATA[index].product}{' '}
-										({value})
-									</text>
-								)
-							}}
-						>
-							{DATA.map((_, index) => (
-								<Cell
-									key={`cell-${index}`}
-									fill={COLORS[index][systemTheme]}
-									className="stroke-background hover:opacity-80"
-								/>
-							))}
-						</Pie>
-					</PieChart>
-				</ResponsiveContainer>
+									return (
+										<text
+											x={x}
+											y={y}
+											className="fill-muted-foreground text-xs"
+											textAnchor={x > cx ? 'start' : 'end'}
+											dominantBaseline="central"
+										>
+											{popularProducts[index].product.length > 12
+												? popularProducts[index].product
+														.substring(0, 12)
+														.concat('...')
+												: popularProducts[index].product}{' '}
+											({value})
+										</text>
+									)
+								}}
+							>
+								{popularProducts.map((_, index) => (
+									<Cell
+										key={`cell-${index}`}
+										fill={COLORS[index][systemTheme]}
+										className="stroke-background hover:opacity-80"
+									/>
+								))}
+							</Pie>
+						</PieChart>
+					</ResponsiveContainer>
+				)}
 			</CardContent>
 		</Card>
 	)
