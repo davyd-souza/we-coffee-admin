0f827105ca736e29877cb591271d426653dd662f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-04-25 08:39:36 -0300

    feat: implement automatic logout
    If user's cookie expire it'll now automatically log him out.

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,11 +1,38 @@
+import { useEffect } from 'react'
+import { isAxiosError } from 'axios'
+import { Outlet, createFileRoute, useNavigate } from '@tanstack/react-router'
+
 import { Header } from '@/components/header'
-import { Outlet, createFileRoute } from '@tanstack/react-router'
+
+import { api } from '@/lib/axios'
 
 export const Route = createFileRoute('/_app')({
 	component: AppLayout,
 })
 
 export function AppLayout() {
+	const navigate = useNavigate({ from: '/' })
+
+	useEffect(() => {
+		const interceptorId = api.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				if (isAxiosError(error)) {
+					const status = error.response?.status
+					const code = error.response?.data.code
+
+					if (status === 401 && code === 'UNAUTHORIZED') {
+						navigate({ to: '/sign-in', replace: true })
+					}
+				}
+			},
+		)
+
+		return () => {
+			api.interceptors.response.eject(interceptorId)
+		}
+	}, [navigate])
+
 	return (
 		<div className="grid grid-rows-app">
 			<Header />
