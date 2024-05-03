9afd6777159be8efa23abee725c172f0388a9f1c
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2024-05-03 17:09:45 -0300

    chore: setup test dependencies

---
+++ src/components/orders/order-status.spec.tsx
@@ -0,0 +1,5 @@
+import { expect, it } from 'vitest'
+
+it('should 1 + 1 equal 2', () => {
+	expect(1 + 1).toEqual(2)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,10 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	test: {
+		setupFiles: './test/setup.ts',
+		environment: 'happy-dom',
+	},
+} as UserConfig & {
+	test: InlineConfig
 })
